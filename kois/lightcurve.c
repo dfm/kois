d933cd6d77c5fb2e4bc40e741c52bdc74f15aad3
Dan F-M <danfm@nyu.edu>
2013-12-25 17:05:41 -0500

    recursively integrate exposure times for fun and profit

--- kois/lightcurve.c
+++ kois/lightcurve.c
@@ -4,79 +4,65 @@
 #include "lightcurve.h"
 #include "quad.h"
 
+#define LC_FACTOR 0.166666666666666666666666666
+
+double lc_evaluate_one (double t, double p, double t0, double tau, double ror,
+                        double b, double mu1, double mu2)
+{
+    double opr = 1+ror, hp = 0.5*p, x;
+    if (b >= opr) return 1.0;
+    x = 2*(fmod(t+hp-t0, p)-hp)*sqrt(opr*opr - b*b)/tau;
+    x = ldlc(ror, sqrt(b*b + x*x), mu1, mu2);
+    return x;
+}
+
+double lc_evaluate (double t, int np, double *periods, double *epochs,
+                    double *durations, double *rors, double *impacts,
+                    double mu1, double mu2)
+{
+    int k;
+    double f = 1.0;
+    for (k = 0; k < np; ++k)
+        f *= lc_evaluate_one(t, periods[k], epochs[k], durations[k], rors[k],
+                             impacts[k], mu1, mu2);
+    return f;
+}
+
+double lc_integrate (double t, double f0, double texp, int np,
+                     double *periods, double *epochs, double *durations,
+                     double *rors, double *impacts, double mu1, double mu2,
+                     double tol, int max_depth, int depth)
+{
+    double tp = t+LC_FACTOR*texp,
+           tm = t-LC_FACTOR*texp,
+           fp = lc_evaluate (tp, np, periods, epochs, durations,
+                             rors, impacts, mu1, mu2),
+           fm = lc_evaluate (tm, np, periods, epochs, durations,
+                             rors, impacts, mu1, mu2);
+    double d = fabs(fp- 2*f0 + fm);
+    if (d > tol && depth < max_depth) {
+        fp = lc_integrate(tp, fp, texp/3.0, np, periods, epochs, durations,
+                          rors, impacts, mu1, mu2, tol, depth+1, max_depth);
+        f0 = lc_integrate(t, f0, texp/3.0, np, periods, epochs, durations,
+                          rors, impacts, mu1, mu2, tol, depth+1, max_depth);
+        fm = lc_integrate(tm, fm, texp/3.0, np, periods, epochs, durations,
+                          rors, impacts, mu1, mu2, tol, depth+1, max_depth);
+    }
+    return (f0+fp+fm) / 3.0;
+}
+
 void lightcurve (int n, double *t, int K, double texp, int np,
                  double *periods, double *epochs, double *durations,
                  double *rors, double *impacts, double mu1, double mu2,
                  double *f)
 {
-    int i, j, k, ntot = n * K;
-    double t1, delta_t, v, b, b2, opr2, t0, P, hp, hpmt0, duration,
-           *ttmp, *ftmp;
+    int i;
+    double f0;
 
-    if (K > 1) {
-        ttmp = malloc(ntot * sizeof(double));
-        ftmp = malloc(ntot * sizeof(double));
-    } else {
-        ttmp = t;
-        ftmp = f;
-    }
-
-    for (i = 0; i < ntot; ++i) ftmp[i] = 1.0;
-    if (K > 1) {
-        for (i = 0; i < n; ++i)
-            for (j = 0; j < K; ++j)
-                ttmp[i*K+j] = t[i] + ((j+0.5)/K - 0.5)*texp;
-    }
-
-    for (k = 0; k < np; ++k) {
-        // Extract the planet parameters.
-        opr2 = 1 + rors[k];
-        opr2 *= opr2;
-        b = impacts[k];
-        b2 = b*b;
-        t0 = epochs[k];
-        P = periods[k];
-        hp = 0.5*P;
-        hpmt0 = hp - t0;
-        duration = durations[k];
-
-        // Skip planets with no transits.
-        if (opr2 > b2) {
-            // // Compute the velocity based on the b=0 duration.
-            // v = 2.0 / duration;
-            //
-            // // Compute the b=impact transit time.
-            // delta_t = sqrt(opr2 - b2) / v;
-
-            // FIXME.
-            delta_t = 0.5 * duration;
-            v = 2 * sqrt(opr2 - b2) / duration;
-
-            // Compute the impact parameter as a function of time.
-            for (i = 0; i < ntot; ++i) {
-                // Find the time since transit.
-                t1 = fmod(ttmp[i] + hpmt0, P) - hp;
-
-                // If the planet is in transit, compute the impact parameter.
-                if (fabs(t1) < delta_t) {
-                    t1 *= v;
-                    ftmp[i] *= ldlc(rors[k], sqrt(b2 + t1*t1), mu1, mu2);
-                }
-            }
-        }
-    }
-
-    // Integrate over exposure time.
-    if (K > 1) {
-        for (i = 0; i < n; ++i) {
-            f[i] = 0.0;
-            for (j = 0; j < K; ++j)
-                f[i] += ftmp[i*K+j] / K;
-        }
-    }
-
-    if (K > 1) {
-        free(ttmp);
-        free(ftmp);
+    for (i = 0; i < n; ++i) {
+        f0 = lc_evaluate(t[i], np, periods, epochs, durations, rors, impacts,
+                         mu1, mu2);
+        f[i] = lc_integrate(t[i], f0, texp, np, periods, epochs, durations,
+                            rors, impacts, mu1, mu2, 1e-7, 0, 4);
     }
 }
