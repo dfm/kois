d421f30f57ac149c0629c19367a3b5b0ef727a9e
Dan F-M <danfm@nyu.edu>
2013-12-27 19:01:11 +0000

    small typo in recursive formula

--- kois/lightcurve.c
+++ kois/lightcurve.c
@@ -4,8 +4,6 @@
 #include "lightcurve.h"
 #include "quad.h"
 
-#define LC_FACTOR 0.166666666666666666666666666
-
 double lc_evaluate_one (double t, double p, double t0, double tau, double ror,
                         double b, double mu1, double mu2)
 {
@@ -33,19 +31,20 @@ double lc_integrate (double t, double f0, double texp, int np,
                      double *rors, double *impacts, double mu1, double mu2,
                      double tol, int max_depth, int depth)
 {
-    double tp = t+LC_FACTOR*texp,
-           tm = t-LC_FACTOR*texp,
+    double st = texp/3.0,
+           tp = t+st,
+           tm = t-st,
            fp = lc_evaluate (tp, np, periods, epochs, durations,
                              rors, impacts, mu1, mu2),
            fm = lc_evaluate (tm, np, periods, epochs, durations,
                              rors, impacts, mu1, mu2);
     double d = fabs(fp- 2*f0 + fm);
     if (d > tol && depth < max_depth) {
-        fp = lc_integrate(tp, fp, texp/3.0, np, periods, epochs, durations,
+        fp = lc_integrate(tp, fp, st, np, periods, epochs, durations,
                           rors, impacts, mu1, mu2, tol, depth+1, max_depth);
-        f0 = lc_integrate(t, f0, texp/3.0, np, periods, epochs, durations,
+        f0 = lc_integrate(t, f0, st, np, periods, epochs, durations,
                           rors, impacts, mu1, mu2, tol, depth+1, max_depth);
-        fm = lc_integrate(tm, fm, texp/3.0, np, periods, epochs, durations,
+        fm = lc_integrate(tm, fm, st, np, periods, epochs, durations,
                           rors, impacts, mu1, mu2, tol, depth+1, max_depth);
     }
     return (f0+fp+fm) / 3.0;
