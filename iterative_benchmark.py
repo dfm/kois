1bd18817df466f0d996bbcf85baed9cf1b34621e
Dan F-M <danfm@nyu.edu>
2014-01-12 08:28:46 -0500

    testing scalings w various parameters

--- iterative_benchmark.py
+++ iterative_benchmark.py
@@ -6,8 +6,25 @@ from __future__ import (division, print_function, absolute_import,
 
 __all__ = []
 
+import time
+import kplr
 import numpy as np
 
+from kois.kois import Model
+from kois.lightcurve.ld import QuadraticLimbDarkening
+
 P = 150.0
-t = np.concatenate([i*P + np.arange(0.0, 10.0, 0.5/24.) for i in range(4)])
-print(t)
+t = np.concatenate([i*P + np.arange(0.0, 10.0, 0.5/24.) for i in range(100)])
+
+ldp = QuadraticLimbDarkening(0.3, 0.1)
+model = Model("test", ldp, tol=1e-20, max_depth=10000)
+model.add_koi(P, 5.0, 13.0 / 24.0, 0.5, 0.8)
+
+print(model.durations)
+texp = kplr.EXPOSURE_TIMES[1]/86400.0
+
+for tau in [0.05, 0.1, 0.5, 1.0]:
+    model.durations[0] = tau
+    strt = time.time()
+    lc = model.get_light_curve(t, texp=texp)
+    print(tau, time.time() - strt)
