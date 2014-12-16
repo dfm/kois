1fee3e07ec0e656bbe08f4fb1173a11bad06fbd5
Dan F-M <danfm@nyu.edu>
2014-12-16 15:22:29 -0500

    fresh start

--- README.md
+++
@@ -1,6 +0,0 @@
-kois
-====
-
-Nothing to see here yet.
-
-Copyright 2013 Dan Foreman-Mackey
--- run.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_process(kid):
-    cmd = "scripts/kois-build-model -c butinah --submit {0}".format(kid)
-    print("Running: {0}".format(cmd))
-    subprocess.check_call(cmd, shell=True)
-
-map(run_process, [line.strip() for line in open("targets/kois.txt")])
